4de10b585a6cfff000990d5cea6cb8887a73406b
SNalbandian <56788693+SNalbandian@users.noreply.github.com>
2020-09-19 13:30:03 +0300

    Add files via upload

---
+++ Vugen_script_5_unfinished_order/Action.c
@@ -0,0 +1,188 @@
+Action()
+{
+	lr_start_transaction("UC_5_Unfinished_order");
+	
+	lr_start_transaction("goToSite");
+
+	//web_set_sockets_option("SSL_VERSION", "2&3");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"none");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"document");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+///*Correlation comment - Do not change!  Original value='129648.213527744zzziHHQpQiDDDDDDtAAVcpzHQQ' Name ='userSession' Type ='ResponseBased'*/
+//	web_reg_save_param_attrib(
+//		"ParamName=userSession",
+//		"TagName=input",
+//		"Extract=value",
+//		"Name=userSession",
+//		"Type=hidden",
+//		SEARCH_FILTERS,
+//		"IgnoreRedirections=No",
+//		"RequestUrl=*/nav.pl*",
+//		LAST);
+
+web_reg_save_param_ex(
+        "ParamName=userSession",
+        "LB=<input type=\"hidden\" name=\"userSession\" value=\"",
+        "RB=\"",
+        SEARCH_FILTERS,
+        "IgnoreRedirections=off",
+        "Scope=Body",
+        "RequestUrl=*",
+        LAST);
+	
+	
+	web_reg_find("Text=signOff=true",
+		LAST);
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t5.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	
+	lr_end_transaction("goToSite", LR_AUTO);
+	
+	lr_start_transaction("login");
+
+	web_revert_auto_header("Sec-Fetch-User");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"same-origin");
+
+	lr_think_time(25);
+	
+	web_reg_find("Fail=NotFound",
+		"Text=User password was correct",
+		LAST);
+	
+	web_submit_data("login.pl",
+		"Action=http://localhost:1080/cgi-bin/login.pl",
+		"Method=POST",
+		"TargetFrame=body",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home",
+		"Snapshot=t6.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=userSession", "Value={userSession}", ENDITEM,
+		"Name=username", "Value={login}", ENDITEM,
+		"Name=password", "Value={password}", ENDITEM,
+		"Name=login.x", "Value=54", ENDITEM,
+		"Name=login.y", "Value=5", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		LAST);
+
+	lr_end_transaction("login",LR_AUTO);
+
+	lr_start_transaction("click_flights");
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	lr_think_time(36);
+
+	web_url("Search Flights Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=search", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t7.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("click_flights",LR_AUTO);
+
+	lr_start_transaction("find_flight");
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	lr_think_time(46);
+	
+	web_reg_find(lr_eval_string("Text/DIG=From {depart} (#) To {arrive} (#)"),
+		LAST);
+	
+	web_submit_data("reservations.pl", 
+		"Action=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl?page=welcome", 
+		"Snapshot=t8.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=depart", "Value={depart}", ENDITEM, 
+		"Name=departDate", "Value={departDate}", ENDITEM, 
+		"Name=arrive", "Value={arrive}", ENDITEM, 
+		"Name=returnDate", "Value={returnDate}", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=seatPref", "Value={seatPref}", ENDITEM, 
+		"Name=seatType", "Value={seatType}", ENDITEM, 
+		"Name=findFlights.x", "Value=62", ENDITEM, 
+		"Name=findFlights.y", "Value=4", ENDITEM, 
+		"Name=.cgifields", "Value=roundtrip", ENDITEM, 
+		"Name=.cgifields", "Value=seatType", ENDITEM, 
+		"Name=.cgifields", "Value=seatPref", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("find_flight",LR_AUTO);
+
+	lr_start_transaction("click_log_out");
+
+	web_revert_auto_header("Sec-Fetch-User");
+
+	web_add_header("Sec-Fetch-User", 
+		"?1");
+
+	lr_think_time(40);
+	
+	web_reg_find("Text=Welcome to the Web Tours site.",
+		LAST);
+
+	web_url("SignOff Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=flights", 
+		"Snapshot=t9.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("click_log_out",LR_AUTO);
+
+	lr_end_transaction("UC_5_Unfinished_order",LR_AUTO);
+
+	return 0;
+}
